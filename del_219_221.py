2f5270bf8e87fc40279e5052870ffeb667eb1d76
hang <2275574766@qq.com>
2023-09-21 10:52:19 +0800

    删除结局相关藏品

--- del_219_221.py
+++ del_219_221.py
@@ -5,8 +5,8 @@ with open('list/win_score.pickle', 'rb') as f:
 with open('list/lose_score.pickle', 'rb') as f:
     lst_lose_score = pickle.load(f)
 
-lst_win_score.append(100)
-lst_lose_score.append(100)
+del lst_win_score[219:222]
+del lst_lose_score[219:222]
 
 with open('list/win_score.pickle', 'wb') as f:
     pickle.dump(lst_win_score, f)
